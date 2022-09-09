3e34970ca183b3d1d08231aba5a4952db42872dc
Autobuntu <rh166617@gmail.com>
2022-09-09 15:23:11 -0400

    REAL LIVE GONGS HERE

--- gong.py
+++ gong.py
@@ -2,13 +2,9 @@
 import random
 from playsound import playsound
 
-# Picks an gong gong iamge
+# Picks an gong gong iamge and sound
 gong_gong_image_int = random.randint(1,6)
 gong_gong_image_pick = 'gong_gong_image_lib/gong_' + str(gong_gong_image_int) + '.txt'
-
-# Picks a gong gong sound
-gong_gong_gong_int = random.randint(1,6)
-gong_gong_gong_pick = 'gong_gong_gong_lib/gong_' + str(gong_gong_gong_int) + '.wav'
 
 # Opens the gong gong image
 f = open(gong_gong_image_pick, 'r')
