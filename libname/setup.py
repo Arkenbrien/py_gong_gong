67d2c4cceb69cb6628cfb538f863825e8de569ce
Autobuntu <rh166617@ohio.edu>
2023-05-04 16:46:32 -0400

    make pip?

---
+++ libname/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='py_gong_gong',
+    version='1.0.0',    
+    description='GONG GONG',
+    url='https://github.com/Arkenbrien/py_gong_gong',
+    author='Rhett Huston',
+    packages=['py_gong_gong'],
+    install_requires=['random',
+                      'playsound'],
+    classifiers=[
+        'Development Status :: 3 - Complete',  
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3'
+    ],
+)
