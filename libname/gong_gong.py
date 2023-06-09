6c6b0ea8a8f5b55951e103bee5ee2ae4133d5aaf
Autobuntu <rh166617@ohio.edu>
2023-06-09 10:05:45 -0400

    actually a function now

--- libname/gong_gong.py
+++ libname/gong_gong.py
@@ -1,21 +1,23 @@
-# Import the gong gong files
-import random
-from playsound import playsound
+def gong_gong():
 
-# Picks an gong gong iamge and sound (7 is danger lel)
-gong_gong_rand_int =  random.randint(1,7)
-gong_gong_image_pick = 'gong_gong_image_lib/gong_' + str(gong_gong_rand_int) + '.txt'
-gong_gong_gong_pick = 'gong_gong_gong_lib/gong_' + str(gong_gong_rand_int) + '.wav'
+    # Import the gong gong files
+    import random
+    from playsound import playsound
 
-# Opens the gong gong image
-f = open(gong_gong_image_pick, 'r')
-lines = f.readlines()
+    # Picks an gong gong iamge and sound (7 is danger lel)
+    gong_gong_rand_int =  random.randint(1,7)
+    gong_gong_image_pick = 'gong_gong_image_lib/gong_' + str(gong_gong_rand_int) + '.txt'
+    gong_gong_gong_pick = 'gong_gong_gong_lib/gong_' + str(gong_gong_rand_int) + '.wav'
 
-# Displays the gong gong image
-count = 0
-for line in lines:
-    count += 1
-    print("{}".format(line.strip()))
+    # Opens the gong gong image
+    f = open(gong_gong_image_pick, 'r')
+    lines = f.readlines()
 
-# Plays the gong gong gong
-playsound(gong_gong_gong_pick)
+    # Displays the gong gong image
+    count = 0
+    for line in lines:
+        count += 1
+        print("{}".format(line.strip()))
+
+    # Plays the gong gong gong
+    playsound(gong_gong_gong_pick)
